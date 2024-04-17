a8615cd98111657b2877a80aeb2df12ace845cbf
Victor Burton <73307686+vburton0@users.noreply.github.com>
2024-04-17 11:04:05 +0000

    Location

--- srcs/Location.cpp
+++ srcs/Location.cpp
@@ -53,193 +53,129 @@ Location::~Location(void)
 
 }
 
-Location &Location::operator=(const Location & other)
+Location &Location::operator=(const Location & src)
 {
-	this->_return = other._return;
-	this->methods = other.methods;
-	this->autoIndex = other.autoIndex;
-	this->root = other.root;
-	this->indexFiles = other.indexFiles;
-	this->bodySize = other.bodySize;
-	this->cgi = other.cgi;
+	this->_return = src._return;
+	this->methods = src.methods;
+	this->autoIndex = src.autoIndex;
+	this->root = src.root;
+	this->indexFiles = src.indexFiles;
+	this->bodySize = src.bodySize;
+	this->cgi = src.cgi;
 	return (*this);
 }
 
-// ************************************************************************** //
-//                                  Private                                   //
-// ************************************************************************** //
-
-void Location::compareBlockInfo(std::string line)
-{
-	std::cout << "line: " << line << std::endl;
-	if (line[0] == '#')
-		;
-	else if (line[line.size() - 1] != ';' || line.find(';') != line.size() - 1)
-		throw Webserv::InvalidFileContentException();
-	else if (!line.compare(0, 5, "root "))
-	{
-		this->root = line.substr(5, line.size() - 6 - (line[line.size() - 2] == ' '));
-		if (this->root.find(' ') != std::string::npos || !this->root.compare(";"))
-			throw Webserv::InvalidFileContentException();
-	}
-	else if (!line.compare(0, 6, "index "))
-	{
-		line = line.substr(6);
-		if (line.empty() || !line.compare(";"))
-			throw Webserv::InvalidFileContentException();
-		while (!line.empty())
-		{
-			size_t size = line.find(' ');
-			if (size == std::string::npos)
-			{
-				if (line.compare(";"))
-					this->indexFiles.push_back(line.substr(0, line.size() - 1));
-				line = "";
-			}
-			else
-			{
-				this->indexFiles.push_back(line.substr(0, size));
-				line = line.substr(size + 1);
-			}
-		}
-	}
-	else if (!line.compare("client_max_body_size 0;") || !line.compare("client_max_body_size 0 ;"))
-	{
-		if (this->_bodySighted)
-			throw Webserv::InvalidFileContentException();
-		this->_bodySighted = true;
-		this->bodySize = 0;
-	}
-	else if (!line.compare(0, 21, "client_max_body_size "))
-	{
-		if (this->_bodySighted)
-			throw Webserv::InvalidFileContentException();
-		this->_bodySighted = true;
-		std::istringstream iss(line.substr(21));
-		int toint;
-		iss >> toint;
-		if (iss.fail() || !toint)
-			throw Webserv::InvalidFileContentException();
-		this->bodySize = toint;
-		size_t index = 21;
-		while (std::isdigit(line[index]))
-			++index;
-		if ((index == line.size() - 2 && line[index] == 'M') || (index == line.size() - 3 && line[index] == 'M' && line[index + 1] == ' '))
-			return ;
-		throw Webserv::InvalidFileContentException();
-	}
-	else if (!line.compare(0, 10, "autoindex "))
-	{
-		if (this->_autoSighted)
-			throw Webserv::InvalidFileContentException();
-		this->_autoSighted = true;
-		line = line.substr(10, line.size() - 11 - (line[line.size() - 2] == ' '));
-		if (line.find(' ') != std::string::npos || !line.compare(";"))
-			throw Webserv::InvalidFileContentException();
-		if (!line.compare("on"))
-			this->autoIndex = true;
-		else if (!line.compare("off"))
-			this->autoIndex = false;
-		else
-			throw Webserv::InvalidFileContentException();
-	}
-	else if (!line.compare(0, 14, "allow_methods "))
-	{
-		line = line.substr(14);
-		if (line.empty() || !line.compare(";"))
-			throw Webserv::InvalidFileContentException();
-		while (!line.empty())
-		{
-			size_t size = line.find(' ');
-			if (size == std::string::npos)
-			{
-				if (line.compare(";"))
-					this->methods.push_back(line.substr(0, line.size() - 1));
-				line = "";
-			}
-			else
-			{
-				this->methods.push_back(line.substr(0, size));
-				line = line.substr(size + 1);
-			}
-		}
-	}
-	else if (!line.compare(0, 4, "cgi "))
-	{
-		if (!this->cgi.empty())
-			throw Webserv::InvalidFileContentException();
-		line = line.substr(4, line.size() - 5 - (line[line.size() - 2] == ' '));
-		if (line.find(' ') != std::string::npos || !line.compare(";"))
-			throw Webserv::InvalidFileContentException();
-		this->cgi = line;
-	}
-	else if (!line.compare(0, 7, "return "))
-	{
-		if (this->_lineSighted)
-			throw Webserv::InvalidFileContentException();
-		this->_return = line.substr(7, line.size() - 8 - (line[line.size() - 2] == ' '));
-		if (this->_return.find(' ') != std::string::npos || !this->_return.compare(";"))
-			throw Webserv::InvalidFileContentException();
-		this->_returnSighted = true;
-	}
+void Location::trimSpaceAndSemicolon(std::string& str) {
+    size_t semicolonPos = str.find(';');
+    if (semicolonPos != std::string::npos) {
+        str.erase(semicolonPos);
+    }
+    str.erase(str.find_last_not_of(" \t\n\r\f\v") + 1);
 }
 
-void Location::checkSetDefault(void)
-{
-	if (!this->_return.empty())
-		return ;
-	if (this->root.empty())
-		throw Webserv::InvalidFileContentException();
-	for (size_t index = 0; index < this->methods.size(); index++)
-	{
-		for (size_t sub_index = 0; sub_index < index; sub_index++)
-		{
-			if (this->methods[index] == this->methods[sub_index])
-				throw Webserv::InvalidFileContentException();
-		}
-		if (this->methods[index] != "GET" && this->methods[index] != "PUT" && this->methods[index] != "POST"
-			&& this->methods[index] != "DELETE" && this->methods[index] != "HEAD")
-			throw Webserv::InvalidFileContentException();
-	}
-	if (this->methods.empty())
-	{
-		this->methods.push_back("GET");
-		this->methods.push_back("PUT");
-		this->methods.push_back("POST");
-		this->methods.push_back("DELETE");
-		this->methods.push_back("HEAD");
-	}
-	std::sort(this->methods.begin(), this->methods.end());
+void Location::addToList(std::string& line, std::list<std::string>& list) {
+    std::istringstream iss(line);
+    std::string item;
+    while (iss >> item) {
+        if (item.back() == ';') {
+            item.pop_back();
+            if (item.empty()) {
+                throw Webserv::InvalidFileContentException();
+            }
+        }
+        list.push_back(item);
+    }
+    if (list.empty()) {
+        throw Webserv::InvalidFileContentException();
+    }
+}
+
+void Location::compareBlockInfo(std::string line) {
+    std::cout << "line: " << line << std::endl;
+    if (line.empty() || line[0] == '#')
+        return;
+
+    // Remove comments and trim line
+    size_t commentPos = line.find('#');
+    if (commentPos != std::string::npos) {
+        line.erase(commentPos);
+    }
+    trimSpaceAndSemicolon(line);
+
+    size_t spacePos = line.find(' ');
+    if (spacePos == std::string::npos) {
+        throw Webserv::InvalidFileContentException();
+    }
+
+    std::string directive = line.substr(0, spacePos);
+    std::string content = line.substr(spacePos + 1);
+
+    if (directive == "listen" || directive == "server_name" || directive == "root" || directive == "index" ||
+        directive == "cgi" || directive == "upload_path" || directive == "return") {
+        std::cout << directive << ": " << content << std::endl;
+    } else if (directive == "client_max_body_size") {
+        std::istringstream iss(content);
+        std::string sizeStr;
+        char unit;
+        if (!(iss >> sizeStr >> unit) || (unit != 'M' && unit != 'K' && unit != ';')) {
+            throw Webserv::InvalidFileContentException();
+        }
+        this->bodySize = std::stoul(sizeStr) * (unit == 'M' ? 1024 * 1024 : 1024);
+    // } else if (directive == "error_page") {
+    //     this->errorPages.push_back(content);
+    } else if (directive == "allow_methods") {
+        std::istringstream iss(content);
+        std::string method;
+        while (iss >> method) {
+            this->methods.push_back(method);
+        }
+    } else if (directive == "directory_listing" || directive == "autoindex") {
+        autoIndex = (content == "on");
+    }else {
+        throw Webserv::InvalidFileContentException(); // Unknown directive
+    }
 }
 
 
-// ************************************************************************** //
-//                                  Public                                    //
-// ************************************************************************** //
+void Location::checkSetDefault(void) {
+    if (!_return.empty()) {
+        return;
+    }
+    if (root.empty()) {
+        throw Webserv::InvalidFileContentException();
+    }
+    static const std::set<std::string> validMethods = {"GET", "PUT", "POST", "DELETE", "HEAD"};
+    std::set<std::string> seenMethods;
 
-void Location::showLocationContent(void)
-{
-	std::cout << "\t-location: " << this->location << std::endl;
-	std::cout << "\t  -root: " << this->root << std::endl;
-	std::cout << "\t  -autoIndex: ";
-	(this->autoIndex)
-		? std::cout << "on" << std::endl
-		: std::cout << "off" << std::endl;
-	std::cout << "\t  -index: ";
-	std::list<std::string>::iterator it = this->indexFiles.begin();
-	std::list<std::string>::iterator ite = this->indexFiles.end();
-	for (; it != ite; it++)
-		std::cout << *it << ' ';
-	std::cout << std::endl;
-	if (this->bodySize != std::string::npos)
-		std::cout << "\t  -bodySize: " << this->bodySize << std::endl;
-	std::cout << "\t  -methods: ";
-	std::vector<std::string>::iterator iit = this->methods.begin();
-	std::vector<std::string>::iterator iite = this->methods.end();
-	for (; iit != iite; iit++)
-		std::cout << *iit << ' ';
-	std::cout << std::endl;
-	std::cout << "\t  -suffixed: " << this->suffixed << std::endl;
-	std::cout << "\t  -cgi: " << this->cgi << std::endl;
-	std::cout << "\t  -return: " << this->_return << std::endl;
+    for (const auto& method : methods) {
+        // Check for duplicate and validate method
+        if (!seenMethods.insert(method).second || validMethods.find(method) == validMethods.end()) {
+            throw Webserv::InvalidFileContentException();
+        }
+    }
+
+    // Set default methods if none are provided
+    if (methods.empty()) {
+        methods = {"GET", "PUT", "POST", "DELETE", "HEAD"};
+    } else {
+        std::sort(methods.begin(), methods.end());
+    }
+}
+
+
+void Location::showLocationContent(void) {
+    std::cout << "\t-location: " << location << std::endl;
+    std::cout << "\t  -root: " << root << std::endl;
+    std::cout << "\t  -autoIndex: " << (autoIndex ? "on" : "off") << std::endl;
+    std::cout << "\t  -index: ";
+    printList(indexFiles);
+    std::cout << std::endl;
+    if (bodySize != std::string::npos) {
+        std::cout << "\t  -bodySize: " << bodySize << std::endl;
+    }
+    std::cout << "\t  -methods: ";
+    printList(methods);
+    std::cout << std::endl;
+    std::cout << "\t  -suffixed: " << suffixed << std::endl;
+    std::cout << "\t  -cgi: " << cgi << std::endl;
 }
